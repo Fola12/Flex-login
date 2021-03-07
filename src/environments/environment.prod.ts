3c038f865d5774cc4afa0a3786d2751fb8a4afbe
Fola12 <okesolafola@gmail.com>
2021-03-07 20:42:55 +0100

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCvTYk1hvJW0XmLT6A3icnBthlQBjjitrs",
+    authDomain: "flex-bab99.firebaseapp.com",
+    projectId: "flex-bab99",
+    storageBucket: "flex-bab99.appspot.com",
+    messagingSenderId: "671896551818",
+    appId: "1:671896551818:web:697d6572bbbecb20adc873",
+    measurementId: "G-1641SKM0T2"
+  }
+
 };
